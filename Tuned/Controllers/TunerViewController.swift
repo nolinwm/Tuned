0e7a7977f8803faad8611868942e74f73214113b
Nolin McFarland <nolin.mcfarland@icloud.com>
2022-05-20 12:50:18 -0400

    Create tuning display and associated animations

--- Tuned/Controllers/TunerViewController.swift
+++ Tuned/Controllers/TunerViewController.swift
@@ -9,14 +9,29 @@ import UIKit
 
 class TunerViewController: UIViewController {
 
-    @IBOutlet weak var noteLabel: UILabel!
+    @IBOutlet weak var tuningDisplay: UIView!
     @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var baseNoteLabel: UILabel!
+    @IBOutlet weak var pitchNoteLabel: UILabel!
+    @IBOutlet weak var baseCircle: UIImageView!
+    @IBOutlet weak var inTuneCircle: UIImageView!
+    
     var tuner: Tuner?
+    var animatingDidTuneNote = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTuner()
     }
+    
+    func showError(_ message: String) {
+        errorLabel.text = message
+        errorLabel.isHidden = false
+    }
+}
+
+// MARK: - Tuner Methods
+extension TunerViewController: TunerDelegate {
     
     func setupTuner() {
         do {
@@ -34,19 +49,58 @@ class TunerViewController: UIViewController {
         }
     }
     
-    func showError(_ message: String) {
-        errorLabel.text = message
-        errorLabel.isHidden = false
+    func tuner(_ tuner: Tuner, didReceive data: TunerData) {
+        guard let note = data.note, let pitchDifference = data.pitchDifference else {
+            resetDisplay()
+            return
+        }
+        updateDisplay(noteName: note.name, pitchDifference: pitchDifference)
+        if abs(pitchDifference) < 1 {
+            didTuneNote()
+        }
     }
 }
 
-extension TunerViewController: TunerDelegate {
-    func tuner(_ tuner: Tuner, didReceive data: TunerData) {
-        guard let note = data.note, let pitchDifference = data.pitchDifference else {
-            noteLabel.text = nil
-            return
+// MARK: - TuningDisplay Methods
+extension TunerViewController {
+    
+    func resetDisplay() {
+        baseNoteLabel.text = ""
+        pitchNoteLabel.text = ""
+        pitchNoteLabel.transform = .identity
+    }
+    
+    func updateDisplay(noteName: String, pitchDifference: Float) {
+        baseNoteLabel.text = noteName
+        pitchNoteLabel.text = noteName
+        
+        UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseOut) {
+            self.pitchNoteLabel.transform = CGAffineTransform(
+                translationX: CGFloat(pitchDifference * abs(pitchDifference)),
+                y: 0
+            )
         }
-        noteLabel.text = note.name
-        print(pitchDifference)
+    }
+    
+    func didTuneNote() {
+        guard !animatingDidTuneNote else { return }
+        animatingDidTuneNote = true
+        
+        inTuneCircle.transform = CGAffineTransform(scaleX: 0, y: 0)
+        inTuneCircle.alpha = 0
+        
+        UIView.animate(withDuration: 0.3, delay: 0) {
+            self.inTuneCircle.alpha = 0.25
+        }
+        
+        UIView.animate(withDuration: 0.6, delay: 0, options: .curveEaseIn) {
+            self.inTuneCircle.transform = CGAffineTransform(scaleX: 1, y: 1)
+        } completion: { complete in
+            self.animatingDidTuneNote = false
+        }
+        
+        UIView.animate(withDuration: 0.15, delay: 0.6) {
+            self.inTuneCircle.alpha = 0
+        }
     }
 }
